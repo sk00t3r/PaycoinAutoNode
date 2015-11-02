26e2287057c0cf37167785fd0a900deb07d8d511
sk00t3r <johnsone@roadrunner.com>
2015-11-02 12:32:20 -0500

    Update PaycoinAutoNode.sh

--- PaycoinAutoNode.sh
+++ PaycoinAutoNode.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 echo "### Change to home directory"
 cd ~
-echo "### Installing sudo"
-apt-get install sudo -y
 echo "### Update Ubuntu"
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
-sudo apt-get install software-properties-common python-software-properties unzip ufw python-pip apache2 -y
-echo "### Installing python-bitcoinrpc"
-sudo pip install python-bitcoinrpc
-echo "### Allow ports 22, 80 & 8998 and enable The Uncomplicated Firewall"
+sudo apt-get install software-properties-common python-software-properties unzip ufw -y
+echo "### Allow ports 22, 8998 and enable The Uncomplicated Firewall"
 sudo ufw allow 22/tcp
-sudo ufw allow 80/tcp
 sudo ufw allow 8998/tcp
 sudo ufw --force enable
 echo "### Creating Swap File"
@@ -36,28 +31,51 @@ echo "### Installing Paycoin Core 0.3.2.0"
 unzip linux64.zip
 rm -f -r linux64.zip
 rm -f -r paycoin-qt
+echo "### Scheduling Cron Job to run Paycoin Core on boot"
+(crontab -l ; echo "@reboot ~/./paycoind")| crontab -
+echo "### System will now reboot"
+reboot
+echo "### Changing to paycoind directory"
+cd ~
+echo "### Stopping Paycoin Server"
+./paycoind stop
 echo "### Changing to home directory"
 cd ~
-echo "### IN 30 SECONDS PLEASE WRITE DOWN YOUR RPCUSER AND RPCPASSWORD AND PRESS CTRL+X (WE WILL NEED THESE SOON)"
-sleep 10
+echo "### Updating Ubuntu"
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get dist-upgrade -y
+sudo apt-get install python-pip apache2 -y
+echo "### Allow ports 80 and enable The Uncomplicated Firewall"
+sudo ufw allow 80/tcp
+sudo ufw --force enable
+echo "### Installing python-bitcoinrpc"
+sudo pip install python-bitcoinrpc
+echo "### Changing to paycoind directory"
+cd ~
+echo "### Starting Paycoin Server"
+./paycoind
+echo "### Changing to home directory"
+cd ~
+echo "### IN 5 SECONDS PLEASE WRITE DOWN YOUR RPCUSER AND RPCPASSWORD AND PRESS CTRL+X (WE WILL NEED THESE SOON)"
+sleep 5
 cd .paycoin
 nano paycoin.conf
 echo "### Changing to home directory"
 cd ~
-echo "###STOPING SERVICES"
-./paycoind stop
-sudo service apache2 stop
 echo "### Downloading the Web Interface"
 mkdir ~/WebInterface/
 cd ~/WebInterface/
 wget -O uptime.py https://raw.githubusercontent.com/sk00t3r/PaycoinAutoNode/master/uptime.py
 wget -O WebInterface.py https://raw.githubusercontent.com/sk00t3r/PaycoinAutoNode/master/WebInterface.py
-echo "### IN 30 SECONDS CONFIGURE WEBINTERFACE.PY WITH RPC_USER, RPC_PASS, NODE_LOCATION, NODE_NAME, NODE_IP, DONATION_XPY_ADDR AND EXIT (DON’T FORGET TO SAVE)"
-sleep 10
+echo "### IN 5 SECONDS CONFIGURE WEBINTERFACE.PY WITH RPC_USER, RPC_PASS, NODE_LOCATION, NODE_NAME, NODE_IP, DONATION_XPY_ADDR AND EXIT (DON’T FORGET TO SAVE)"
+sleep 5
 nano WebInterface.py
 echo "### Installing the Web Interface"
 sudo python WebInterface.py
-echo "### Scheduling Cron Job to run Paycoin Core on boot and WebInterface.py every 5 minutes"
+echo "### Changing to home directory"
+cd ~
+echo "### Scheduling Cron Job to run WebInterface.py every 5 minutes and Paycoin Core on boot"
 (crontab -l ; echo "@reboot ~/./paycoind")| crontab -
 (crontab -l ; echo "*/5 * * * * sudo python ~/WebInterface/WebInterface.py")| crontab -
 echo "### System will now reboot"
