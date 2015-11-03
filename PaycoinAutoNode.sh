05ceab404dd53d2f07dc445f0c40473889426b9b
sk00t3r <johnsone@roadrunner.com>
2015-11-03 00:22:50 -0500

    Update PaycoinAutoNode.sh

--- PaycoinAutoNode.sh
+++ PaycoinAutoNode.sh
@@ -65,7 +65,7 @@ echo "### Downloading the Web Interface"
 mkdir ~/WebInterface/
 cd ~/WebInterface/
 wget -O cpu.py https://raw.githubusercontent.com/sk00t3r/PaycoinAutoNode/1-step-install/cpu.py
-wget -O uptime.py https://raw.githubusercontent.com/sk00t3r/PaycoinAutoNode/master/uptime.py
+wget -O server_uptime.py https://raw.githubusercontent.com/sk00t3r/PaycoinAutoNode/1-step-install/server_uptime.py
 wget -O WebInterface.py https://raw.githubusercontent.com/sk00t3r/PaycoinAutoNode/master/WebInterface.py
 echo "### IN 15 SECONDS CONFIGURE WEBINTERFACE.PY WITH RPC_USER, RPC_PASS, NODE_LOCATION, NODE_NAME, NODE_IP, DONATION_XPY_ADDR AND EXIT (DON’T FORGET TO SAVE)"
 sleep 15
