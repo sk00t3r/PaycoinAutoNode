fd704f28ae990e933613c7ba82912b3e22e7042f
sk00t3r <johnsone@roadrunner.com>
2015-11-03 20:41:31 -0500

    Update osversion.py

--- osversion.py
+++ osversion.py
@@ -2,6 +2,6 @@
 
 import platform
 
-def osver():
-  os=platform.dist()
-  return os
+distro = platform.linux_distribution()[0]
+version = platform.linux_distribution()[1]
+name =  platform.linux_distribution()[2]
