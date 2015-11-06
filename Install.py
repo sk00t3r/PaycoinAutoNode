5187f87fd45c543ecf1de16e30880704144947d5
sk00t3r <johnsone@roadrunner.com>
2015-11-05 23:24:30 -0500

    Create Install.py

---
+++ Install.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+import urllib
+import platform
+import subprocess
+import os
+
+findos = (platform.linux_distribution()[0])
+
+if findos == "Ubuntu" or "Debian":
+        url = "https://raw.githubusercontent.com/sk00t3r/PaycoinAutoNode/1-step-install/PaycoinAutoNodeDeb.sh"
+        file_name = "PaycoinAutoNodeDeb.sh"
+        urllib.urlretrieve(url, file_name)
+		os.chmod("/opt/PaycoinAutoNodeDeb.sh", 755)
+		subprocess.call("./PaycoinAutoNodeDeb.sh")
+
+if findos == "CentOS Linux":
+        url = "https://raw.githubusercontent.com/sk00t3r/PaycoinAutoNode/1-step-install/PaycoinAutoNodeRehl.sh"
+        file_name = "PaycoinAutoNodeRehl.sh"
+        urllib.urlretrieve(url, file_name)
+		os.chmod("/opt/PaycoinAutoNodeRehl.sh", 755)
+		subprocess.call("./PaycoinAutoNodeRehl.sh")
+
+else:
+        print "Operating System Not Supported, Please Install Manually"
+		
+
+
+
+
